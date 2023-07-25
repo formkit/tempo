d1fb57cad2f2c1c59f7912905efbb52cb6e9130e
Justin Schroeder <justin@formkit.com>
2023-07-25 16:46:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tempo
+
+Tempo takes the pain out of dates in JavaScript by allowing you to parse, manipulate, format, and internationalize dates and times using JavaScript’s native `Intl.DateTimeFormat`.
---
+++ rollup.config.js
@@ -0,0 +1,59 @@
+import typescript from "@rollup/plugin-typescript"
+import { terser } from "rollup-plugin-terser"
+
+const FRAMEWORK = process.env.FRAMEWORK || "index"
+const DECLARATIONS = process.env.DECLARATIONS || false
+const MIN = process.env.MIN || false
+
+const external = [
+  "vue",
+  "preact",
+  "react",
+  "angular",
+  "process",
+  "solid-js",
+  "rollup-plugin-terser",
+  "../index",
+]
+
+function createOutput() {
+  if (DECLARATIONS) {
+    return {
+      dir: "./dist",
+      format: "esm",
+    }
+  }
+  return {
+    file: `./dist/${FRAMEWORK !== "index" ? FRAMEWORK + "/" : ""}index.${
+      MIN ? "min.js" : "mjs"
+    }`,
+    format: "esm",
+  }
+}
+
+const plugins = [
+  typescript({
+    tsconfig: "tsconfig.json",
+    compilerOptions: DECLARATIONS
+      ? {
+          declaration: true,
+          emitDeclarationOnly: true,
+        }
+      : {},
+    rootDir: "./",
+    outDir: `./dist`,
+    include: ["./src/**/*"],
+    exclude: ["./docs"],
+  }),
+]
+
+if (MIN) {
+  plugins.push(terser())
+}
+
+export default {
+  external,
+  input: `./src/${FRAMEWORK === "index" ? "" : FRAMEWORK + "/"}index.ts`,
+  output: createOutput(),
+  plugins,
+}
