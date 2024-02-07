6339a6a051ed7573f7651380be9cd3ce7082aa77
Justin Schroeder <justin@formkit.com>
2024-02-07 16:59:36 -0500

    chore: documents ap, dayOfYear, formatStr, fourDigitYear, monthDays, nearestDay

--- docs/components/content/Data.vue
+++ docs/components/content/Data.vue
@@ -14,11 +14,71 @@ const fns = {
       },
     ],
   },
-  dayOfYear: {},
-  formatStr: {},
-  fourDigitYear: {},
-  monthDays: {},
-  nearestDay: {},
+  dayOfYear: {
+    description: `Gets the what day of the year a given date is. For example, August 1st is the 213th day of the year on non-leapyears and 214th on leapyears.`,
+    arguments: [
+      {
+        name: "date",
+        type: "Date",
+      },
+    ],
+    return: "number",
+  },
+  formatStr: {
+    description:
+      "This little gem of a function returns the token format for a given format style.",
+    arguments: [
+      {
+        name: "format",
+        type: "{ date?: string, time?: string }",
+      },
+      {
+        name: "locale?",
+        type: "string",
+      },
+    ],
+    return: "string",
+  },
+  fourDigitYear: {
+    description:
+      "Converts a 2 digit year into a 4 digit year. This function assumes years 20 years into the future belong to the current century, and the past 80 are in the past.",
+    arguments: [
+      {
+        name: "year",
+        type: "string",
+      },
+    ],
+    return: "number",
+  },
+  monthDays: {
+    description: "Returns the number of days in a given month.",
+    arguments: [
+      {
+        name: "date",
+        type: "Date",
+      },
+    ],
+    return: "number",
+  },
+  nearestDay: {
+    description:
+      "Performs a bidirectional search for the nearest date that passes a given search function. It stops searching when it finds a result or when it reaches the constraint bounds (on both sides).",
+    arguments: [
+      {
+        name: "date",
+        type: "Date",
+      },
+      {
+        name: "searchFunction",
+        type: "(date: Date) => boolean",
+      },
+      {
+        name: "constraint",
+        type: 'number | "month" | "week" | "year" = 7',
+      },
+    ],
+    return: "Date | null",
+  },
   offset: {},
   range: {},
   sameDay: {},
