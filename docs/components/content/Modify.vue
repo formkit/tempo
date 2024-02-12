0f416089d3c0ce785366ee1c22edfb0c1b78a6d1
Justin Schroeder <justin@formkit.com>
2024-02-12 13:57:21 -0500

    feat: adds docsearch

--- docs/components/content/Modify.vue
+++ docs/components/content/Modify.vue
@@ -266,7 +266,7 @@ const fns: Record<
       do not change the date argument).
     </p>
     <div v-for="(def, fn) in fns">
-      <h3>{{ fn }}</h3>
+      <h3 :id="fn">{{ fn }}</h3>
       <FunctionReference
         :function="fn"
         :arguments="def.arguments"
