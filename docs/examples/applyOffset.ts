31cb19ad73c065ddd1de22d3b97eeffd0634f400
hrynevych.romann <hrynevych.romann@gmail.com>
2024-02-23 12:30:57 +0200

    docs: add date log to `applyOffset` example

--- docs/examples/applyOffset.ts
+++ docs/examples/applyOffset.ts
@@ -1,9 +1,12 @@
-import { offset, applyOffset } from "@formkit/tempo"
+import { offset, applyOffset, date } from "@formkit/tempo"
 
 // 💡 Pickup at 9:30 in Europe/Lisbon
 
 // Notice that the time has no offset, thus it is "local":
 const d = "2025-03-25 09:30"
+
+// Convert the string to a date (just for example):
+date(d)
 
 // Lisbon is at UTC+0:
 const lisbonOffset = offset(d, "Europe/Lisbon")
