a63552dcebd71fc52e4521fecd68068cf9383faf
Roman Hrynevych <82209198+romanhrynevych@users.noreply.github.com>
2024-02-23 21:00:02 +0200

    fix: remove TSDoc inside `isBefore`
    Co-authored-by: Justin Schroeder <justin@formkit.com>

--- src/isBefore.ts
+++ src/isBefore.ts
@@ -2,11 +2,6 @@ import { date } from "./date"
 import type { DateInput } from "./types"
 
 /**
- * @name isBefore
- * @category Common Helpers
- * @summary Is the first date before the second one?
- *
- * @description
  * Is the first date before the second one?
  *
  * @param inputDate - The date that should be before the other one to return true
