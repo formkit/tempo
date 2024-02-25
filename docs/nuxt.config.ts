7e1fb2a9e6ca35ffc242d90566a87bacaaf75cfc
hrynevych.romann <hrynevych.romann@gmail.com>
2024-02-25 14:48:40 +0200

    chore: revert `nuxt.config.ts` nitro configuration

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -100,14 +100,14 @@ export default defineNuxtConfig({
     },
   },
   debug: true,
-  // nitro: {
-  //   storage: {
-  //     kv: {
-  //       driver: process.env.KV_DRIVER,
-  //       accountId: process.env.KV_ACCOUNT_ID,
-  //       namespaceId: process.env.KV_NAMESPACE_ID,
-  //       apiToken: process.env.KV_API_TOKEN,
-  //     },
-  //   },
-  // },
+  nitro: {
+    storage: {
+      kv: {
+        driver: process.env.KV_DRIVER,
+        accountId: process.env.KV_ACCOUNT_ID,
+        namespaceId: process.env.KV_NAMESPACE_ID,
+        apiToken: process.env.KV_API_TOKEN,
+      },
+    },
+  },
 })
