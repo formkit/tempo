3a985aea4ea5198aa2cb91f3cca9bb388ceb2a6a
hisayuki_mori <h.mori0206@gmail.com>
2024-03-06 22:06:20 +0900

    chore: add comment and type

--- src/deviceTZ.ts
+++ src/deviceTZ.ts
@@ -1,6 +1,8 @@
 /**
  * Get the timezone of the device.
+ *
+ * * Note: If the environment variable TZ is not set, it will return undefined.
  */
-export function deviceTZ() {
+export function deviceTZ(): string | undefined {
   return Intl.DateTimeFormat().resolvedOptions().timeZone
 }
