73b49910ee9e798b36b7e01d0c751bd4cfa96c9a
lehuygiang28 <lehuygiang28@gmail.com>
2024-03-07 13:43:06 +0700

    fix(tzDate): :bug: fix wrong timezone test
    as New_York is UTC-5 and Amsterdam is UTC+1 so different of hour is +6 not -2

--- src/__tests__/tzDate.spec.ts
+++ src/__tests__/tzDate.spec.ts
@@ -5,7 +5,7 @@ process.env.TZ = "America/New_York"
 describe("tzDate", () => {
   it("can can parse an unzoned/offset iso8601 as if it was in a timezone", () => {
     expect(tzDate("2017-05-06T12:00", "Europe/Amsterdam").toISOString()).toBe(
-      "2017-05-06T10:00:00.000Z"
+      "2017-05-06T18:00:00.000Z"
     )
   })
 })
