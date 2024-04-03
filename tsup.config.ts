c1fd5aba886e7beeb5223b0797fca9c0a518c0cd
WilcoSp <Wilcosp@hotmail.nl>
2024-04-03 04:05:55 +0200

    build: bundles fns as individual modules (#49)
    * creating a split & bundled builds that can be used
    (include dist currently for testing, when merging add it back to ignore and remove it from github)
    
    * changing package name to have less confusion in visualizer
    
    * last one to include pre build
    
    * removing pre build & preparing for pull request
    
    * changed tsup config to have commonjs be build as a bundle like before to get the file size down
    added exports for browser/unpkg

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,44 @@
 import { defineConfig } from "tsup"
+import { esbuildPluginFilePathExtensions as fpe } from "esbuild-plugin-file-path-extensions"
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  dts: true,
-  outExtension: ({ format }) => ({ js: format === "cjs" ? ".cjs" : ".mjs" }),
-})
+export default defineConfig([
+  // for bundlers like vite, rollup, esbuild, webpack etc
+  {
+    entry: ["src/**.ts"],
+    format: ["esm"],
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    dts: true,
+    outExtension: ({ format }) => ({ js: format === "cjs" ? ".cjs" : ".mjs" }),
+    esbuildPlugins: [fpe()],
+  },
+  // common js for node and other backend runtimes
+  {
+    entry: ["src/index.ts"],
+    format: ["cjs"],
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    dts: true,
+    outExtension: ({ format }) => ({
+      js: format === "cjs" ? ".cjs" : ".mjs",
+    }),
+  },
+
+  // bundle js for browsers/cdn
+  {
+    entry: {
+      bundle: "src/index.ts",
+    },
+    format: ["esm"],
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    dts: true,
+
+    outExtension: ({ format }) => ({
+      js: format === "cjs" ? ".cjs" : ".mjs",
+    }),
+  },
+])
