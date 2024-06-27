98bab94368fc888ec71de0a54d4a87b29df1b9c8
Isak Wang Gustavsen <69854945+isakgustavsen@users.noreply.github.com>
2024-06-27 03:33:14 +0200

    Added sameMonth check (#60)
    * add sameMonth check

---
+++ src/__tests__/sameMonth.spec.ts
@@ -0,0 +1,21 @@
+import { describe, it, expect } from "vitest"
+import { sameMonth } from "../sameMonth"
+process.env.TZ = "America/New_York"
+
+describe("sameMonth", () => {
+  it("can determine two dates are the exact same", () => {
+    expect(sameMonth(new Date(), new Date())).toBe(true)
+  })
+
+  it("can determine string dates", () => {
+    expect(sameMonth("2023-02-02 13:20:30", "2024-02-01 12:21:32")).toBe(true)
+  })
+
+  it("can determine different dates and time except months", () => {
+    expect(sameMonth("2023-02-02 13:20:30", "2024-01-01 12:21:32")).toBe(false)
+  })
+
+  it("can determine different dates and time with same months", () => {
+    expect(sameMonth("2023-01-01 10:10:30", "2024-01-02 11:12:00")).toBe(true)
+  })
+})
