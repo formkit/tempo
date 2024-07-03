ab2191344c2f738f9a63cc35836eef433d0de830
WilcoSp <wilcosp@hotmail.nl>
2024-07-03 19:38:22 +0200

    same* now also support optional date arguments

--- src/sameSecond.ts
+++ src/sameSecond.ts
@@ -1,12 +1,19 @@
 import { date } from "./date"
-import type { DateInput } from "./types"
+import type { DateInput, MaybeDateInput } from "./types"
 
 /**
  * Checks if two date objects refer to the same time seconds. Ignores date.
  * @param inputDateA - First date to compare
+ * @param [inputDateB] - Second date to compare or the current time if nothing given
+ */
+export function sameSecond(inputDateA: DateInput, inputDateB?: MaybeDateInput): boolean
+/**
+ * Checks if two date objects refer to the same time seconds. Ignores date.
+ * @param [inputDateA] - First date to compare or the current time if null given
  * @param inputDateB - Second date to compare
  */
-export function sameSecond(inputDateA: DateInput, inputDateB: DateInput) {
+export function sameSecond(inputDateA: DateInput, inputDateB: DateInput): boolean
+export function sameSecond(inputDateA: DateInput, inputDateB?: MaybeDateInput): boolean {
   const a = date(inputDateA)
   const b = date(inputDateB)
   return a.getSeconds() === b.getSeconds()
--- src/sameYear.ts
+++ src/sameYear.ts
@@ -1,12 +1,22 @@
 import { date } from "./date"
-import type { DateInput } from "./types"
+import type { DateInput, MaybeDateInput } from "./types"
 
 /**
  * Checks if two date objects refer to the same year.
  * @param inputDateA - First date to compare
+ * @param [inputDateB] - Second date to compare or the current time if null given
+ */
+export function sameYear(inputDateA: DateInput, inputDateB?: MaybeDateInput): boolean
+/**
+ * Checks if two date objects refer to the same year.
+ * @param [inputDateA] - First date to compare or the current time if null given
  * @param inputDateB - Second date to compare
  */
-export function sameYear(inputDateA: DateInput, inputDateB: DateInput) {
+export function sameYear(inputDateA: MaybeDateInput, inputDateB: DateInput): boolean
+export function sameYear(
+  inputDateA: MaybeDateInput,
+  inputDateB?: MaybeDateInput
+): boolean {
   const a = date(inputDateA)
   const b = date(inputDateB)
   return a.getFullYear() === b.getFullYear()
