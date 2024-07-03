97172ad3b5ae50b088d138a6c81f7e9c5594a07b
WilcoSp <wilcosp@hotmail.nl>
2024-07-03 13:06:36 +0200

    isBefore & isAfter doc change from now -> the current time changed isEqual to accept 1 optional date

--- src/isAfter.ts
+++ src/isAfter.ts
@@ -4,15 +4,15 @@ import type { DateInput, MaybeDateInput } from "./types"
 /**
  * @name isAfter
  * @category Common Helpers
- * @summary Is the first date after the second one or `now`?
+ * @summary Is the first date after the second one or the current time?
  *
  * @description
- * Is the first date after the second one or `now`?
+ * Is the first date after the second one or the current time?
  *
  * @param inputDate - The date that should be after the other one to return true
- * @param dateToCompare - The date to compare with or `now` if nothing given
+ * @param [dateToCompare] - The date to compare with or the current time if nothing given
  *
- * @returns The first date is after the second date or `now`.
+ * @returns The first date is after the second date or the current time.
  */
 export function isAfter(inputDate: DateInput, dateToCompare?: MaybeDateInput) {
   const _date = date(inputDate)
--- src/isBefore.ts
+++ src/isBefore.ts
@@ -2,12 +2,12 @@ import { date } from "./date"
 import type { DateInput, MaybeDateInput } from "./types"
 
 /**
- * Is the first date before the second one or `now`?
+ * Is the first date before the second one or the current time?
  *
  * @param inputDate - The date that should be before the other one to return true
- * @param dateToCompare - The date to compare with or `now` if nothing given
+ * @param [dateToCompare] - The date to compare with or the current time if nothing given
  *
- * @returns The first date is before the second date or `now`.
+ * @returns The first date is before the second date or the current time.
  */
 export function isBefore(inputDate: DateInput, dateToCompare?: MaybeDateInput): boolean {
   const _date = date(inputDate)
