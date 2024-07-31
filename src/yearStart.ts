6ae532a05edaac91669e51a90080e7234d2c4bf7
WilcoSp <wilcosp@hotmail.nl>
2024-07-31 11:38:20 +0200

    yearStart inputDate to maybe type

--- src/yearStart.ts
+++ src/yearStart.ts
@@ -1,11 +1,11 @@
 import { date } from "./date"
-import type { DateInput } from "./types"
+import type { MaybeDateInput } from "./types"
 
 /**
  * Returns a Date object for the with the input date set to the start of the current year.
  * @param [inputDate] - A string, Date object or nothing for the current year
  */
-export function yearStart(inputDate?: DateInput): Date {
+export function yearStart(inputDate?: MaybeDateInput): Date {
   const d = date(inputDate)
 
   d.setMonth(0, 1)
