95dfd25242fc7c10b7d94d4f3ac86326e668cfbc
WilcoSp <wilcosp@hotmail.nl>
2024-09-18 15:38:25 +0200

    moving the date over flow handling to a separate function

---
+++ src/handleDateOverflow.ts
@@ -0,0 +1,23 @@
+import { date } from "./date"
+import { monthDays } from "./monthDays"
+import { MaybeDateInput } from "./types"
+
+export function handleOverflow(
+  inputDate: MaybeDateInput | undefined,
+  allowOverflow = false,
+  action: (d: Date) => void
+) {
+  let d = date(inputDate)
+  const dayOfMonth = d.getDate()
+  // If overflowing is disallowed, set the date back to the first of the month
+  if (!allowOverflow) d.setDate(1)
+  // d.setMonth(d.getMonth() + count)
+  action(d)
+  // If overflowing is disallowed, we need to set the date back to the proper
+  // day or the last day of the month.
+  if (!allowOverflow) {
+    const daysInMonth = monthDays(d)
+    d.setDate(daysInMonth < dayOfMonth ? daysInMonth : dayOfMonth)
+  }
+  return d
+}
