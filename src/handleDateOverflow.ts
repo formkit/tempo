7c899f55d34d41c64f114a2c66f982367aac8b0c
WilcoSp <wilcosp@hotmail.nl>
2024-10-12 12:37:03 +0200

    adding jsdoc for handleOverflow

--- src/handleDateOverflow.ts
+++ src/handleDateOverflow.ts
@@ -2,6 +2,13 @@ import { date } from "./date"
 import { monthDays } from "./monthDays"
 import { MaybeDateInput } from "./types"
 
+/**
+ * handles date overflow when changing month or year
+ * @param [inputDate] - A string, Date object or null for the current time
+ * @param action - the action that changes the month/year
+ * @param [dateOverflow] - Whether or not to allow the date to overflow to another month if the inputDate’s month is out of range of the new month.
+ * @returns a new Date object with the change year/month without overflowing (except if wanted to overflow)
+ */
 export function handleOverflow(
   inputDate: MaybeDateInput | undefined,
   action: (d: Date) => void,
