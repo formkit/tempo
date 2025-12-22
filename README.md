e7b41bddbd19d75d029a5b50f6a10759ef0d80df
Justin Schroeder <justin@formkit.com>
2025-12-22 13:00:06 -0500

    docs: small language tweak

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Tempo is a new library in a proud tradition of JavaScript date and time librarie
 
 Tempo is best thought of as a collection of utilities for working with `Date` objects — an important distinction from other libraries that provide custom date primitives. Under the hood, Tempo mines JavaScript's `Intl.DateTimeFormat` to extract complex data like timezones offsets and locale aware date formats giving you a simple API to format, parse, and manipulates dates.
 
-Tempo is tiny tree-shakable framework, you can only take what you need. All functionality is available in **5.2 kB for esm** and **5.5 kB for cjs** modules (minified and brotlied). [Size Limit](https://github.com/ai/size-limit) controls the size.
+Tempo is tiny tree-shakable framework, you can only take what you need. You can work with timezones with only a few bytes of library code, or perform full international date formatting for ~2Kb (minified and brotlied). [Size Limit](https://github.com/ai/size-limit) controls the size.
 
 <a href="https://tempo.formkit.com">
 <img src="docs/public/read-the-docs.png" alt="Read the docs" width="200" height="43">
--- docs/components/content/Introduction.vue
+++ docs/components/content/Introduction.vue
@@ -4,19 +4,18 @@
   <PageSection>
     <HeadingSection title="Introduction" class="text-sky-500" />
     <p>
-      Tempo is a new library in a proud tradition of JavaScript date and time
-      libraries. Inspired by the likes of moment.js, day.js, and date-fns, Tempo
-      is built from the ground up to be as small and easy to use as possible
-      (all features ~5kB compressed) — including first-class support for
-      timezone operations.
+      Tempo is a new library in a proud tradition of JavaScript date and time libraries.
+      Tempo is tiny and tree-shakable. You can work with timezones with only a few bytes
+      of library code, or perform full international date formatting for ~2Kb (minified
+      and brotlied). Each function on this page details it's own impact on your code size.
     </p>
     <p>
-      Tempo is best thought of as a collection of utilities for working with the
-      native <code>Date</code> object — an important distinction from other
-      libraries that provide custom date primitives. Under the hood, Tempo mines
-      JavaScript's <code>Intl.DateTimeFormat</code> to extract complex data like
-      timezones offsets and locale aware date formats, giving you a simple API
-      to format, parse, and manipulate dates.
+      Tempo is best thought of as a collection of utilities for working with the native
+      <code>Date</code> object — an important distinction from other libraries that
+      provide custom date primitives. Under the hood, Tempo mines JavaScript's
+      <code>Intl.DateTimeFormat</code> to extract complex data like timezones offsets and
+      locale aware date formats, giving you a simple API to format, parse, and manipulate
+      dates.
     </p>
 
     <CodeExample file="introduction" />
